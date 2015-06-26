d1656a758b9456a0464b0b869f4748425026ea8f
Grace Ahmed <graceahmed@fq-130-49-43-218.wireless.pitt.edu>
2015-06-25 21:10:34 -0400

    day 3 something

---
+++ day3.py
@@ -0,0 +1,16 @@
+#day3.py
+#we are going to learn to write more python
+
+names = ['corey', 'philip', 'daniel', 'rose']
+print names
+print names[1]
+
+if 1 < 0:
+    print names[2]
+else:
+    print "blah blah"
+    
+for current_number in range(1,10):
+    print current_number  
+age_of_dog = input('Enter age of dog: ')
+print age_of_dog
